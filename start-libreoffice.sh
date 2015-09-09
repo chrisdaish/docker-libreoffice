760f042f6d4f24d696cb238dc93d9b3d24035059
Chris Daish <chrisdaish@gmail.com>
2015-09-09 12:49:57 +0100

    Added config dir

--- start-libreoffice.sh
+++ start-libreoffice.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 groupmod -g $gid libreoffice
 usermod -u $uid -g $gid libreoffice
+
+if [ -d /home/libreoffice/.config/libreoffice ]; then
+  chown -R libreoffice:libreoffice /home/libreoffice/.config/libreoffice
+fi
+
 exec su -ls "/bin/bash" -c "mkdir -p /home/libreoffice/.local/share; /usr/local/bin/libreoffice5.0" libreoffice
